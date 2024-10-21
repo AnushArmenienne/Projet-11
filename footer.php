7a2156b71f0558780335653dab1d8125f6987582
AnushArmenienne <hayerdepoitou@hotmail.fr>
2024-10-21 22:05:38 +0200

    cours code propre 21/10/2024

--- footer.php
+++ footer.php
@@ -1,41 +1,31 @@
 </main>
-        </div>
-    </div>
 
-    <footer class="site__footer">
-  
-    <?php get_template_part('template-parts/modale', '');?>
+<footer class="site__footer">
 
-    <footer>
+	<?php get_template_part('template-parts/modale', ''); ?>
 
 
-    <?php
-wp_nav_menu( array( 
-    'theme_location' => 'footer-menu',
-    'container_class' => 'footer-menu-class' ) 
-); 
-?>
-
-<p>TOUS DROITS RESERVES</p>
 
 
-            
-    </footer>
+	<?php
+	wp_nav_menu(
+		array(
+			'theme_location' => 'footer-menu',
+			'container_class' => 'footer-menu-class'
+		)
+	);
+	?>
 
-</div>
+	<p>TOUS DROITS RESERVES</p>
+
+
+
+</footer>
+
+
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
-
-
-
-
-
-
-
-
-
-
-
--- header.php
+++ header.php
@@ -1,29 +1,36 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Ma page</title>
-    <link href="style.css" rel="stylesheet">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Ma page</title>
+	<link href="style.css" rel="stylesheet">
 
-    <?php wp_head();?>
-    
+	<?php wp_head(); ?>
+
 </head>
+
 <body>
-    
-<div id="nav">
-    <div id="logo">
 
-<?php echo get_custom_logo();?>
+	<div id="nav">
+		<div id="logo">
 
-    </div>
+			<?php echo get_custom_logo(); ?>
 
-    <div id="menu">
-<?php
-wp_nav_menu( array( 
-    'theme_location' => 'header-menu',
-    'container_class' => 'header-menu-class' ) 
-); 
-?>
-</div>
-</div>
+		</div>
+
+		<div id="menu">
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'header-menu',
+					'container_class' => 'header-menu-class'
+				)
+			);
+			?>
+		</div>
+	</div>
+
+
+	<main>
